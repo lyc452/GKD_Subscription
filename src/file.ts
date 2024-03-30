17bbec67ba1a280e7f511f27d4a041a843214ba2
lyc452 <108231863+lyc452@users.noreply.github.com>
2024-03-30 16:53:38 +0800

    Add files via upload

--- src/file.ts
+++ src/file.ts
@@ -190,26 +190,26 @@ export const checkConfig = (newConfig: RawSubscription) => {
       const oldGroup = oldGroups.find((og) => og.key == g.key);
       if (!oldGroup || !_.isEqual(oldGroup, g)) {
         // 检查新增/变动的规则组是否能被分类
-        if (
-          !categories.some(
-            (c) => g.name == c.name || g.name.startsWith(c.name + '-'),
-          )
-        ) {
-          console.error({
-            configName: newConfig.name,
-            appId: app.id,
-            appName: app.name,
-            groupName: g.name,
-            groupKey: g.key,
-            categories: categories.map((c) => c.name),
-          });
-          throw new Error(
-            [
-              'invalid group name, it must equal any category name or startWith categoryName + "-".',
-              'example: "开屏广告" or "分段广告-朋友圈"',
-            ].join('\n'),
-          );
-        }
+        // if (
+        //   !categories.some(
+        //     (c) => g.name == c.name || g.name.startsWith(c.name + '-'),
+        //   )
+        // ) {
+        //   console.error({
+        //     configName: newConfig.name,
+        //     appId: app.id,
+        //     appName: app.name,
+        //     groupName: g.name,
+        //     groupKey: g.key,
+        //     categories: categories.map((c) => c.name),
+        //   });
+        //   throw new Error(
+        //     [
+        //       'invalid group name, it must equal any category name or startWith categoryName + "-".',
+        //       'example: "开屏广告" or "分段广告-朋友圈"',
+        //     ].join('\n'),
+        //   );
+        // }
       }
 
       if (deprecatedKeys.includes(g.key)) {
