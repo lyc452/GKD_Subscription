33bc9bce879e7245c799833683436cee02a44784
lyc452 <108231863+lyc452@users.noreply.github.com>
2024-05-03 15:27:27 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,14 +4,13 @@ import apps from './rawApps';
 import type { RawSubscription } from './types';
 
 const subsConfig: RawSubscription = {
-  id: 1,
-  version: 1,
-  name: 'GKD订阅',
+  id: 404,
+  version: 0,
+  name: "LYC's GKD Subscription",
   author: 'lyc452',
   supportUri: 'https://github.com/lyc452/subscription',
-  updateUrl: 'https://github.com/lyc452/subscription/blob/main/dist/gkd.json5',
-  checkUpdateUrl:
-    'https://raw.githubusercontent.com/lyc452/subscription/main/dist/gkd.version.json',
+  updateUrl: 'https://raw.githubusercontent.com/lyc452/subscription/main/dist/gkd.json5',
+  checkUpdateUrl: 'https://raw.githubusercontent.com/lyc452/subscription/main/dist/gkd.version.json',
   globalGroups,
   categories,
   apps,
