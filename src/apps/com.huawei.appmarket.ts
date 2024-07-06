28e0e53042e0347c064d3fd2b811270c008a9105
Arthur Edwards <108231863+lyc452@users.noreply.github.com>
2024-07-06 12:43:32 +0800

    feat(华为应用市场): 开屏广告

--- src/apps/com.huawei.appmarket.ts
+++ src/apps/com.huawei.appmarket.ts
@@ -5,8 +5,23 @@ export default defineAppConfig({
   name: '华为应用市场',
   groups: [
     {
-      name: '通知提示',
       key: 0,
+      name: '开屏广告',
+      actionMaximum: 1,
+      matchTime: 5000,
+      resetMatch: 'app',
+      rules: [
+        {
+          quickFind: true,
+          activityIds: 'com.huawei.appmarket.MainActivity',
+          matches: '[id="com.huawei.appmarket:id/clock_layout"] > [text*="跳过"]',
+          snapshotUrls: 'https://i.gkd.li/i/16114723',
+        },
+      ],
+    },
+    {
+      name: '通知提示',
+      key: 1,
       quickFind: true,
       matchTime: 10000,
       rules: [
@@ -19,7 +34,7 @@ export default defineAppConfig({
       ],
     },
     {
-      key: 1,
+      key: 2,
       name: '更新提示',
       matchTime: 10000,
       actionMaximum: 1,
