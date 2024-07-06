dec245a33da611137bc01b7b50dd8b3a1cc64af0
Arthur Edwards <108231863+lyc452@users.noreply.github.com>
2024-07-06 08:38:48 +0800

    feat(HMS Core): 开屏广告

--- src/apps/com.huawei.hwid.ts
+++ src/apps/com.huawei.hwid.ts
@@ -5,8 +5,22 @@ export default defineAppConfig({
   name: 'HMS Core',
   groups: [
     {
-      // 我不知道这个广告从哪里来的，它出乎意料地跳出并覆盖在Gspace上面
       key: 0,
+      name: '开屏广告',
+      actionMaximum: 1,
+      matchTime: 5000,
+      resetMatch: 'app',
+      rules: [
+        {
+          quickFind: true,
+          matches: '[id="com.huawei.hwid:id/hiad_skip_text"]',
+          snapshotUrls: 'https://i.gkd.li/i/16114493',
+        },
+      ],
+    },
+    {
+      // 我不知道这个广告从哪里来的，它出乎意料地跳出并覆盖在Gspace上面
+      key: 1,
       name: '全屏广告',
       activityIds: [
         'com.huawei.openalliance.ad.ppskit.activity.InnerPPSInterstitialAdActivity',
