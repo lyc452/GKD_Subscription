91dca48f2a0dfcfba8b871f51708c0419a92a7a6
Arthur Edwards <108231863+lyc452@users.noreply.github.com>
2024-11-23 16:43:40 +0800

    Update com.hxak.liangongbao.ts

--- src/apps/com.hxak.liangongbao.ts
+++ src/apps/com.hxak.liangongbao.ts
@@ -12,5 +12,18 @@ export default defineAppConfig({
       resetMatch: 'app',
       rules: '[id=`com.hxak.liangongbao:id/time_down`]',
     },
+    {
+      key: 1,
+      name: '全屏广告',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: 'com.hxak.liangongbao.login.ui.HomeActivity',
+          matches: '[id="com.hxak.liangongbao:id/iv_close"]',
+          exampleUrls: 'https://e.gkd.li/5755f383-7dc2-40bc-a747-49e324ef7af9',
+          snapshotUrls: 'https://i.gkd.li/i/17770893',
+        },
+      ],
+    },
   ],
 });
