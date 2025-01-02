2a9617cf6bf6021fe4c7ccb518c0ab1ca15feedc
Arthur Edwards <108231863+lyc452@users.noreply.github.com>
2025-01-02 23:08:18 +0800

    Update com.ixyzh.question.ts

--- src/apps/com.ixyzh.question.ts
+++ src/apps/com.ixyzh.question.ts
@@ -17,6 +17,10 @@ export default defineAppConfig({
           matches: '[text="关闭广告"]',
           snapshotUrls: 'https://i.gkd.li/i/17000667',
         },
+        {
+          activityIds: 'com.ixyzh.question.ui.SplashActivity',
+          matches: '[id="com.ixyzh.question:id/action_bar_root"] > [id="android:id/content"] >2 [id="com.ixyzh.question:id/splash_container"] >3 View[id=null]',
+        },
       ],
     },
     {
@@ -32,6 +36,35 @@ export default defineAppConfig({
             'https://i.gkd.li/i/17000681',
             'https://i.gkd.li/i/17000682',
           ],
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '分段广告-首页',
+      rules: [
+        {
+          key: 1,
+          activityIds: 'com.ixyzh.question.HomeActivity',
+          matches: 'ImageView[desc="ad_logo"] - ImageView[desc="close"]',
+        },
+        {
+          key: 2,
+          preKeys: 1,
+          fastQuery: true,
+          activityIds: 'com.ixyzh.question.HomeActivity',
+          matches: '[text="不感兴趣"]',
+        },
+      ],
+    },
+    {
+      key: 3,
+      name: '全屏广告',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: 'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
+          matches: '[id="com.ixyzh.question:id/ll_ad_close"] > [text="关闭广告"]',
         },
       ],
     },
